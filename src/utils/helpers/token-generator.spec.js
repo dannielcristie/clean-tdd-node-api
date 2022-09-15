9988bfc5127e23df4cb2c0109a223045c4fecc39
Danniel Cristie <dany14012@gmail.com>
2022-09-15 19:56:55 -0300

    ensure TokenGenerator calls JWT with correct values

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,11 +1,15 @@
 const jwt = require('jsonwebtoken')
 class TokenGenerator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
   async generate (id) {
-    return jwt.sign(id, 'secret')
+    return jwt.sign(id, this.secret)
   }
 }
 const makeSut = () => {
-  return new TokenGenerator()
+  return new TokenGenerator('secret')
 }
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', async () => {
@@ -19,5 +23,11 @@ describe('Token Generator', () => {
     const sut = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
+  })
+  test('Should call JWT with correct values', async () => {
+    const sut = makeSut()
+    await sut.generate('any_id')
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secret).toBe(sut.secret)
   })
 })
