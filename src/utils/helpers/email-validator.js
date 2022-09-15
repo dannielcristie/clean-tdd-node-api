0f22a4d6ff0e715afd2806dca0870ca3cab4e120
Danniel Cristie <dany14012@gmail.com>
2022-09-15 17:26:31 -0300

    ensure EmailValidator  throws if no email is provided

--- src/utils/helpers/email-validator.js
+++ src/utils/helpers/email-validator.js
@@ -1,6 +1,11 @@
 const validator = require('validator')
+const MissingParamError = require('../errors/missing-param-error')
+
 module.exports = class Emailvalidator {
   isValid (email) {
+    if (!email) {
+      throw new MissingParamError('email')
+    }
     return validator.isEmail(email)
   }
 }
